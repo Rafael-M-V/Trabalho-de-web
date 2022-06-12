af58559b324e6e7c052c076f38c62241944011ff
Lucas Xavier Leite <lucasxavierleite@pm.me>
2022-06-11 21:09:16 -0300

    Add product and section (carousel) components

---
+++ src/ProductsData.js
@@ -0,0 +1,108 @@
+import pera from './components/product/images/pera.png'
+import maca from './components/product/images/maca.png'
+import banana from './components/product/images/banana.jpg'
+
+const productsData = [
+    {
+        id: 1,
+        name: 'Pera',
+        img: pera,
+        price: 6.98,
+        discount: 0.05
+    },
+    {
+        id: 2,
+        name: 'Maçã',
+        img: maca,
+        price: 6.98
+    },
+    {
+        id: 3,
+        name: 'Banana',
+        img: banana,
+        price: 6.98,
+        discount: 0.20
+    },
+    {
+        id: 4,
+        name: 'Pera',
+        img: pera,
+        price: 6.98,
+        discount: 0.05
+    },
+    {
+        id: 5,
+        name: 'Maçã',
+        img: maca,
+        price: 6.98
+    },
+    {
+        id: 6,
+        name: 'Banana',
+        img: banana,
+        price: 6.98,
+        discount: 0.20
+    },
+    {
+        id: 7,
+        name: 'Pera',
+        img: pera,
+        price: 6.98,
+        discount: 0.05
+    },
+    {
+        id: 8,
+        name: 'Maçã',
+        img: maca,
+        price: 6.98
+    },
+    {
+        id: 9,
+        name: 'Banana',
+        img: banana,
+        price: 6.98,
+        discount: 0.20
+    },
+    {
+        id: 10,
+        name: 'Pera',
+        img: pera,
+        price: 6.98,
+        discount: 0.05
+    },
+    {
+        id: 11,
+        name: 'Maçã',
+        img: maca,
+        price: 6.98
+    },
+    {
+        id: 12,
+        name: 'Banana',
+        img: banana,
+        price: 6.98,
+        discount: 0.20
+    },
+    {
+        id: 13,
+        name: 'Pera',
+        img: pera,
+        price: 6.98,
+        discount: 0.05
+    },
+    {
+        id: 14,
+        name: 'Maçã',
+        img: maca,
+        price: 6.98
+    },
+    {
+        id: 15,
+        name: 'Banana',
+        img: banana,
+        price: 6.98,
+        discount: 0.20
+    },
+]
+
+export default productsData;
